a194cb42ff0f7d9d57270f774a65f23e06bdf337
MarvelMcD <MarvelMcD@gmail.com>
2024-04-19 11:56:18 -0700

    start

--- rules.js
+++ rules.js
@@ -1,32 +1,46 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
-        this.engine.addChoice("Begin the story");
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.addChoice("Start");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        if(locationData.Choices != null) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
-            this.engine.addChoice("The end.")
+            this.engine.addChoice("The end.");
         }
     }
 
+    // handleChoice(choice) {
+    //     if(choice) {
+    //         this.engine.show("&gt; "+choice.Text);
+    //         this.engine.gotoScene(Location, choice.Target);
+    //     } else {
+    //         this.engine.gotoScene(End);
+    //     }
+    // }
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
+        if (choice && typeof choice === 'string' && this.engine.storyData.Fuses[choice]) {
+            // Handle collecting the fuse
+            this.engine.show(`You collected ${this.engine.storyData.Fuses[choice]}.`);
+            // Remove the fuse from the location
+            delete this.engine.storyData.Locations[this.engine.currentLocation].Fuse;
+            // Proceed to the next scene or update as needed
+        } else if (choice) {
+            this.engine.show("&gt; " + choice.Text);
             this.engine.gotoScene(Location, choice.Target);
         } else {
             this.engine.gotoScene(End);
